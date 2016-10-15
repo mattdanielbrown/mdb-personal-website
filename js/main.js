724410afe2ab5ae4bb7bdde004c86f74f3199a89
Matthew Daniel Brown <developer@mattdanielbrown.com>
2016-10-15 00:57:24 -0500

    * HTML :       - Changed the JS script reference in Index.html to that of the minified version of the main.js script
       * JS :
          - Added functions to defocus buttons after being clicked.
          - Added functions to defocus links after being clicked.
          - Added functions to defocus form submit input (button) after being clicked.
          - Added smooth scrolling to destination for anchor links.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 jQuery(document).ready(function($){
+
+    /*      VARIABLES       */
+
+    var formSubmitButton = $("#submit-input");
     
-    // Set buttons to de-focus after being clicked
+
+
+    /*      FUNCTIONS       */
+
+
+
+
+    /*      MAIN SCRIPT     */
+
+    /* Set buttons to de-focus after being clicked      */
     $("button").on('click', function(e) {
         this.blur();
     });
+
+    /* Smooth scroll between anchor links (and de-focus them after being clicked)      */
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        this.blur();
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 800);
+                return false;
+            }
+        }
+    });
+
+    /* Defocus submit form button after it's clicked       */
+    formSubmitButton.on('click', function(e) {
+       this.blur();
+    });
+
 
 });
